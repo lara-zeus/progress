f2186ce0933b1b27cbe80222333848d37e2dac05
atmonshi <1952412+atmonshi@users.noreply.github.com>
2025-04-22 14:17:15 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace LaraZeus\Progress\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use LaraZeus\Progress\ProgressServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
