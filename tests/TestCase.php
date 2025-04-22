e8cac88be0ea522f8bff07b9473bfdcf0b57ec35
Ash Monsh <wh7r.com@gmail.com>
2025-04-22 17:16:45 +0300

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,15 +8,6 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'LaraZeus\\Progress\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
@@ -27,10 +18,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_progress_table.php.stub';
-        $migration->up();
-        */
     }
 }
