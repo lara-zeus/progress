83a242851ce7afd434101521b61c46f8790f77bf
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2025-06-23 18:21:54 +0300

    Fix php stan

--- src/Infolists/Components/ProgressEntry.php
+++ src/Infolists/Components/ProgressEntry.php
@@ -38,8 +38,7 @@ class ProgressEntry extends Entry
             $progressColor = '#e74c3c';
         }
 
-        $displayProgress = $progress === 100 ? number_format($progress, 0) : number_format($progress, 2);
-        $displayProgress = (int) round($displayProgress);
+        $displayProgress = number_format($progress);
 
         return [
             'progress' => $progress,
